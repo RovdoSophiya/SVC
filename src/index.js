a9cdca728ba3074a248544a3b85318154847d2dd
Sophiya <rovdosofia@gmal.com>
2024-09-22 21:46:19 +0300

    add: meetDae component, fix: link in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "../src/index.css";
 import "../src/fonts.css";
 import "../src/change-theme.css";
+import "../src/normalize.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
