9ea468ee82915c93587bcb83533c2cafb236d7d4
Sophiya <rovdosofia@gmal.com>
2024-09-22 20:43:47 +0300

    add: css for header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import "../src/index.css";
+import "../src/fonts.css";
+import "../src/change-theme.css";
+import App from "../src/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
