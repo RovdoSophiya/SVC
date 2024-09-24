7958fd9f36e783ff5bbcfe03b6fde5d8208e5162
Sophiya <rovdosofia@gmal.com>
2024-09-24 13:48:08 +0300

    fix: reviews component

--- src/Pages/_components/ReviewsImage/reviewsImage.js
+++ src/Pages/_components/ReviewsImage/reviewsImage.js
@@ -4,17 +4,17 @@ const ReviewImage = () => {
   const Text = ["Reviews", "What our customers say about us"];
 
   return (
-    <section class="reviews-image">
-      <div class="reviews-page-info">
+    <section className="reviews-image">
+      <div className="reviews-page-info">
         <p
-          class="reviews-image-section-text1"
+          className="reviews-image-section-text1"
           id="reviews"
           data-i18="reviewsInfo"
         >
           {Text[0]}
         </p>
         <p
-          class="reviews-image-section-text2"
+          className="reviews-image-section-text2"
           id="reviews-2"
           data-i18="custSay"
         >
