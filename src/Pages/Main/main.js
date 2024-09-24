5f51cbfe7d156305f4fc538baacdc8fd7402bf4e
Sophiya <rovdosofia@gmal.com>
2024-09-24 13:45:22 +0300

    add modal component

--- src/Pages/Main/main.js
+++ src/Pages/Main/main.js
@@ -1,7 +1,10 @@
 import GreenMenu from "../_components/GreenMenu/greenMenu";
 import MeetDae from "../_components/MeetDae/meetDae";
 import ShopNow from "../_components/ShopNow/ShopNow";
+import Modal from "../_components/Modal/Modal";
+import { useState } from "react";
 const Main = () => {
+
   return (
     <div>
       <ShopNow text="SHOP NOW" />
@@ -10,6 +13,7 @@ const Main = () => {
         annotation="Meet Dae"
         description="We’re a haircare line based in sunny Arizona. Our products are made up of the many things that make our home special, including the scent of orange blossoms in the air, colors of majestic sunsets and nourishing ingredients derived from desert botanicals. We believe haircare doesn’t have to be complicated and aimed to make a line that is simple and effective. We hope that dae plays a special role in your everyday hair routine from sunrise to sunset and allows you to feel the warmth of the desert wherever you are."
       />
+      
     </div>
   );
 };
--- src/Pages/_components/ShopNow/ShopNow.js
+++ src/Pages/_components/ShopNow/ShopNow.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
 import "./photo-section.css";
+import Modal from "../Modal/Modal";
+import { useState } from "react";
 
-const ShopNow = ({ text }) => {
+const ShopNow = ({ text}) => {
+   /*состояние, отвечающее за видимость модального окна */
+   const[modalActive, setModalActive]=useState(false);
   return (
+    <>
     <section className="photo-section">
       <div className="shop-now-container">
         <div className="shop-now">
-          <a data-i18="shopText" className="shop-text" href="#">
+          <button className="shop-text" onClick={()=>setModalActive(true)}>
             {text}
-          </a>
+          </button>
         </div>
       </div>
     </section>
+    <Modal active={modalActive} setActive={setModalActive}/>
+    </>
   );
 };
 export default ShopNow;
