390832bfc8bcc65e5d0d2138b05bf9a0d9e86e9d
Sophiya <rovdosofia@gmal.com>
2024-09-29 16:16:05 +0300

    add: fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "../src/assets/css/normalize.css";
+import "../src/assets/css/fonts.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
