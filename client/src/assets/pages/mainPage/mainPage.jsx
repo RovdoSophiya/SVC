8a460bd1c1d6856ed892ee98d456ac82d5c7cb08
Sophiya <rovdosofia@gmal.com>
2024-11-10 17:30:49 +0300

    init: add blog component

--- client/src/assets/pages/mainPage/mainPage.jsx
+++ client/src/assets/pages/mainPage/mainPage.jsx
@@ -1,6 +1,7 @@
 import ParisCup from "../_components/parisCup/parisCup";
 import Collections from "../_components/collections/collections";
 import Partners from "../_components/partners/partners";
+import Blog from "../_components/blog/blog";
 
 const Main = () => {
   return (
@@ -8,6 +9,7 @@ const Main = () => {
       <ParisCup />
       <Collections />
       <Partners />
+      <Blog />
     </div>
   );
 };
