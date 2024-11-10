1c5ae0622752272974d39b745147735417c37fb6
Sophiya <rovdosofia@gmal.com>
2024-11-10 16:55:20 +0300

    init: partners component

--- client/src/assets/pages/mainPage/mainPage.jsx
+++ client/src/assets/pages/mainPage/mainPage.jsx
@@ -1,11 +1,13 @@
 import ParisCup from "../_components/parisCup/parisCup";
 import Collections from "../_components/collections/collections";
+import Partners from "../_components/partners/partners";
 
 const Main = () => {
   return (
     <div>
       <ParisCup />
       <Collections />
+      <Partners />
     </div>
   );
 };
